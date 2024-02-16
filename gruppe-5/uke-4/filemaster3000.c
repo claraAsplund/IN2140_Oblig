63da2bed6d7440a43e0a94d78779896b5f8d4d5a
Philip <philiphe@ifi.uio.no>
2024-02-16 15:15:05 +0100

    added week 4

---
+++ gruppe-5/uke-4/filemaster3000.c
@@ -0,0 +1,55 @@
+#include <string.h>
+#include <stdio.h>
+
+#define BUFSIZE 255
+
+int main(int argc, char **argv){
+
+	FILE *fil;
+
+	char *mode, *filename;
+	char buf[BUFSIZE];
+	int rc, wc;
+
+	if (argc < 2){
+		fprintf(stderr, "too few arguments\n");
+		return 1;
+	}
+
+	mode = argv[1];
+	filename = argv[2];
+
+	fil = fopen(filename, mode);
+	if (fil == NULL){
+		perror("fopen");
+		return 1;
+	}
+
+	if (*mode == 'r'){
+		rc = fread(buf, sizeof(char), BUFSIZE-1, fil);
+		if (rc < BUFSIZE && ferror(fil)){
+			fprintf(stderr, "fread");
+			fclose(fil);
+			return 1;
+		}
+		buf[rc] = 0;
+		printf("%s\n", buf);
+	}
+
+	if (*mode == 'w'){
+		if (argc < 3){
+			fprintf(stderr, "too few arguments");
+			fclose(fil);
+			return 1;
+		}
+		wc = fwrite(argv[3], sizeof(char), strlen(argv[3]), fil);
+		if (wc < (int)strlen(argv[3])){
+			fprintf(stderr, "fwrite");
+			fclose(fil);
+			return 1;
+		}
+	}
+
+	fclose(fil);
+	return 0;
+}
---
+++ gruppe-5/uke-4/readme.md
@@ -0,0 +1,6 @@
+
+# Uke 4
+
+This week I showed you how to deal with files, both buffered and unbuffered.  
+The example for unbuffered you will find in unbuffered.c  
+The example for buffered you will find in the file filemaster3000.c
